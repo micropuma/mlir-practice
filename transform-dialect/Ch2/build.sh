46715b1728407b962c9612bd11828d0c2b4b5d1a
micropuma <micropuma@163.com>
2025-04-10 17:16:46 +0800

    update build script

--- transform-dialect/Ch2/build.sh
+++ transform-dialect/Ch2/build.sh
@@ -3,6 +3,7 @@ cd build/
 cmake -G Ninja ../ \
    -DLLVM_USE_LINKER=lld \
    -DCMAKE_BUILD_TYPE=Release \
-   -DLLVM_ENABLE_ASSERTIONS=ON 
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DCMAKE_EXPORT_COMPILE_COMMANDS=ON 
 
 ninja -j $(nproc)
